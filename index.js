fff14baf3cbccfa908c56ed64fbbd735c76d5380
Serpent03 <dumbpilot03@gmail.com>
2023-10-21 10:02:31 +0530

    feat: need addition of auto-refreshing display

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import {
   contentPopulation,
 } from "./utils.js";
 
+
 !localStorage.getItem("users") && localStorage.setItem("users", "");
 !localStorage.getItem("posts") && localStorage.setItem("posts", "");
 !sessionStorage.getItem("currentUser") &&
@@ -41,6 +42,7 @@ homePageInjection && // checking if the current page is on the home feed page
       alert("Not logged in!");
       redirectToPage("login");
     }
+    // read all the previously made posts, and display them on the post section
     contentPopulation(homePageInjection);
     // after a redirect back to 'home' by the createNewPost() function,
     // we will reload the page in such a way that we scroll to the latest post automatically,
@@ -57,24 +59,7 @@ postSubmitButton &&
     createNewPost(postTextData, homePageInjection);
   });
 
-// homePageInjection &&
-//   createNewPost(
-//     "Putin worked as a KGB foreign intelligence officer for 16 years, rising to the rank of lieutenant colonel before resigning in 1991 to begin a political career in Saint Petersburg. In 1996, he moved to Moscow to join the administration of President Boris Yeltsin.",
-//     homePageInjection
-//   );
+// check if any new posts have been made(i.e, a change in the localStorage)
+// and automatically refresh the page
 
-// verify that read function is operating
-// as intended
-
-// if homePage has no sessionStorage("user") => redirect to login
-// at login, reference if user && password
-// at register, storage user && password
-
-// why not use sessionStorage for user login, and then use localStorage for
-// post persistence?
-
-// sessionStorage.setItem('currentUser', user.UUID);
-
-// so persistent read and write operations are only going to be supported
-// by localStorage or sessionStorage depending on how long we want to keep it
-// persistent for
+// will probably be done through the window.EventListener("storage") method
